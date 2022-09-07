2483d4fbc036cab20f594d0c20a7788f51c7a721
Brendan <bthoeung@gmail.com>
2022-09-06 20:18:20 -0700

    Initial commit. Created the first window.

---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginPage.fxml"));
+        stage.setTitle("Login Page");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
+    }
+
+    public static void Main(String[] args){
+        launch(args);
+    }
+
+
+}
